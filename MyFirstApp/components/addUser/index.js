c015069b7b656bbaaa67df11bfa212e64007ef5b
matthieu lebon <matthieu.lebon93@gmail.com>
2016-03-24 16:34:56 +0000

    update ( array )

--- MyFirstApp/components/addUser/index.js
+++ MyFirstApp/components/addUser/index.js
@@ -2,7 +2,30 @@
 
 app.addUser = kendo.observable({
     onShow: function () {
-        $('.text').text('lol');
+        addUser();
     },
     afterShow: function () {}
 });
+
+var addUser = function(){
+    $( "#addUserForm" ).submit(function( e ) {
+        e.preventDefault();
+        
+        var username = $('#username').val();
+        var age = $('#age').val();
+        
+        datas.push({
+            url : 'images/01.jpg',
+            name : username,
+            age : age
+        });
+        $('#username').val('');
+        $('#age').val('');
+        
+       $.each( datas, function( key, value ) {
+            $('#img').html('<img src='+datas[0].url +'>');
+          	$('#name').html(datas[key].name);
+          	$('#userAge').html(datas[key].age);
+        });
+   	});
+}
