0e8302f34de9cb1512067292410c9917f005d6d3
matthieu lebon <matthieu.lebon93@gmail.com>
2016-03-24 15:16:32 +0000

    first commit ( base )

--- MyFirstApp/components/home/index.js
+++ MyFirstApp/components/home/index.js
@@ -1,11 +1,40 @@
 'use strict';
 
 app.home = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function () {
+        showList();
+    },
+    afterShow: function () {}
 });
 
-// START_CUSTOM_CODE_home
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+var datas = [
+    {
+        url: "images/01.jpg",
+        name: "Heisenberg",
+        age: 30
+    },
+    {
+        url: "images/02.jpg",
+        name: "Obama",
+        age: 30
+    },
+    {
+        url: "images/03.jpg",
+        name: "Anonymous",
+        age: "N/A"
+    }
+      ];
+var dataSource = new kendo.data.DataSource({
+    transport: {
+        read: function (o) {
+            o.success(datas);
+        }
+    }
+});
 
-// END_CUSTOM_CODE_home
+var showList = function () {
+    $("#usersList").kendoMobileListView({
+        dataSource: dataSource,
+        template: "<div class='img-size'><img src='#: url #'></div><div class='user-info'><p>#: name # </p><p><span>Age: #: age #</span></p></div>"
+    });
+}
